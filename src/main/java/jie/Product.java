fe76bc4545a6bb6e4aa2083f3a9aa1d1765b34b9
qingjie <qzhao02@syr.edu>
2015-01-14 22:17:24 -0700

    Here is source code
    It is my first time to use github. I am not sure it can run on any
    computer, It works on local. I will write a document with screenshots

---
+++ src/main/java/jie/Product.java
@@ -0,0 +1,83 @@
+package jie;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Product {
+	private String id;
+	private String name;
+	@JsonProperty("white70PxImgUrl")
+	private String image;
+	private String brandId;
+	private String brandName;
+	private String brandUrl;
+	private String description;
+	@JsonProperty("numberSoldInLast30Days")
+	private int quantitySold;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getBrandId() {
+		return brandId;
+	}
+
+	public void setBrandId(String brandId) {
+		this.brandId = brandId;
+	}
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public String getBrandUrl() {
+		return brandUrl;
+	}
+
+	public void setBrandUrl(String brandUrl) {
+		this.brandUrl = brandUrl;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getQuantitySold() {
+		return quantitySold;
+	}
+
+	public void setQuantitySold(int quantitySold) {
+		this.quantitySold = quantitySold;
+	}
+
+}
